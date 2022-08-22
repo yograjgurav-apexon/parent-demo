470e69886b446779bbe6a2adb723da36a5904690
yograj-gurav <yograjgurav02@gmail.com>
2022-08-22 21:09:09 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'parent-demo';
+  toggleValue: boolean = true;
+
+  toggle(value: object) {
+    // console.log('value', value);
+    this.toggleValue = !this.toggleValue;
+  }
+
+  // show(value: boolean) {
+  //   console.log('value', value);
+  // }
+
+  greet(name: string) {
+    alert('Hello ' + name);
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css'],
+})
+export class ChildComponent implements OnInit, OnDestroy, OnChanges {
+  @Output() greetEvent = new EventEmitter();
+  @Input() titleName = '';
+  @Output() listItem = new EventEmitter();
+  name: string = 'Raj';
+  isToggled: boolean = true;
+  message?: string;
+
+  displayMessage() {
+    alert(this.message);
+  }
+
+  constructor() {
+    console.log({ constructor: 'Contructor' });
+    this.message = 'Constructor created';
+  }
+
+  toggleList() {
+    this.listItem.emit(this.isToggled);
+  }
+
+  ngOnDestroy(): void {
+    console.log({ onDestroy: 'Destroyed' });
+    // this.message = 'ngOnDestroy created';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log({ onChanges: 'Changed', changes });
+    console.log('onChanges', changes);
+    // this.message = 'ngOnChanges created';
+  }
+
+  ngOnInit(): void {
+    console.log({ onInIt: 'OnInit' });
+  }
+
+  callParentGreet() {
+    this.greetEvent.emit(this.name);
+  }
+}
